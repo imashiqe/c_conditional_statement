0ccc5b79d3b7ef5699a5bd7142bbdf9e3a535032
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-07-04 22:57:27 +0600

    Wrong Marks Implement

--- studentpassfaill.c
+++ studentpassfaill.c
@@ -4,12 +4,14 @@ int main(){
    printf("Enter Number (0-100) :");
    scanf("%d" , &marks);
 
-   if(marks <= 30){
-      printf("Fail \n");
+  if(marks >= 0 && marks <= 30){
+       printf("Fail \n");
 
-   }else{
-    printf("Pass \n");
-   }
+  }else if (marks > 30 && marks <= 100){
+      printf("Pass \n");
+  }else {
+    printf("Wrong Marks");
+  }
 
 
 
