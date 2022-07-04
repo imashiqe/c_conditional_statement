73d5081304cba3e9437b8e758f035d1fd7d96553
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-07-04 16:27:10 +0600

    thank you child

--- chapter03.c
+++ chapter03.c
@@ -5,11 +5,15 @@ int main(){
    printf("enter age :");
    scanf("%d" , &age);
 
-   if(age > 18){
+   if(age >= 18){
        printf("adult \n");
 
    }
 
+   else if (age > 13 && age <18)
+   {
+    printf("teenager \n");
+   }
    else{
        printf("not adult \n");
 
